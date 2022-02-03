8f29cef7379e3b12a2cf04f5667466bd36a109e3
Goran Lesevic <goran.lesevic1@gmail.com>
2022-02-03 17:09:57 +0100

    Test

---
+++ app/src/main/java/com/example/myapplication22/FlashLightActivity.java
@@ -0,0 +1,162 @@
+package com.example.myapplication22;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class FlashLightActivity extends AppCompatActivity {
+    private static final String TAG = "FlashLightActivity";
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private ImageButton mTorchOnOffButton;
+    private Boolean isTorchOn;
+    private MediaPlayer mp;
+    private int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTorchOnOffButton = (ImageButton) findViewById(R.id.button_on_off);
+        isTorchOn = false;
+        count = 0;
+        // Load the add into Admob banner view.
+        // Creating and load a  new InterstitialAd .
+        //Load BannerAd
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isFlashAvailable) {
+
+            AlertDialog alert = new AlertDialog.Builder(FlashLightActivity.this)
+                    .create();
+            alert.setTitle("Error !!");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                    System.exit(0);
+                }
+            });
+            alert.show();
+            return;
+        }
+
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        mTorchOnOffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (isTorchOn) {
+                        turnOffFlashLight();
+                        isTorchOn = false;
+                    } else {
+                        turnOnFlashLight();
+                        isTorchOn = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void turnOnFlashLight() {
+        Log.d(TAG, "turnOnFlashLight()");
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                count++;
+                if (count == 2) {
+                    count = 0;
+
+                }
+                mCameraManager.setTorchMode(mCameraId, true);
+                playOnOffSound();
+
+                mTorchOnOffButton.setImageResource(R.drawable.on);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void turnOffFlashLight() {
+        Log.d(TAG, "turnOffFlashLight()");
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
+                playOnOffSound();
+                mTorchOnOffButton.setImageResource(R.drawable.off);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playOnOffSound() {
+
+        mp = MediaPlayer.create(FlashLightActivity.this, R.raw.flash_sound);
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // TODO Auto-generated method stub
+                mp.release();
+            }
+        });
+        mp.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isTorchOn) {
+            count = 0;
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isTorchOn) {
+            count = 0;
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isTorchOn) {
+            count = 0;
+            turnOnFlashLight();
+        }
+    }
+}
