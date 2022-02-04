866407614a96f176c61e10bdb27db9048470d916
Goran Lesevic <goran.lesevic1@gmail.com>
2022-02-04 12:27:38 +0100

    Test

--- app/src/main/java/com/example/myapplication22/FlashLightActivity.java
+++ app/src/main/java/com/example/myapplication22/FlashLightActivity.java
@@ -26,7 +26,7 @@ public class FlashLightActivity extends AppCompatActivity {
     private MediaPlayer mp;
     private int count;
 
-    @Overridec
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
